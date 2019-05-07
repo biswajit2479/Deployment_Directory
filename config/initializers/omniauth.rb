02ad848674d859602935ab2ac9f062c7899e6e52
biswajit2479 <biswajit.nayak@jifflenow.com>
2019-05-07 16:26:43 +0530

    Updated the auth parameters

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,6 +5,5 @@ OmniAuth.config.logger = Rails.logger
 OmniAuth.config.full_host = 'http://localhost:3000'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, GOOGLE_OAUTH2_CREDS["client_id"], GOOGLE_OAUTH2_CREDS["client_secret"]
-    #provider :google_oauth2, '810659021532-i86d300pjgn8bd27e08u2p9b085966sq.apps.googleusercontent.com', 'BPuW8GXrL6hmT-vNJa82oB5v'
+  provider :google_oauth2, GOOGLE_OAUTH2_CREDS["client_id"], GOOGLE_OAUTH2_CREDS["client_secret"],:skip_jwt => true
 end
